d989dc50de369138f0cd1996ebe42bebaac73e45
Mali <lindatrana06@gmail.com>
2025-07-13 20:12:32 -0600

    Inicie la vista de cesta. Agregue la funcion verCesta que recorre el array obtenido del localStorage.

--- script/cestaJs.js
+++ script/cestaJs.js
@@ -5,8 +5,39 @@
         tempDiv.innerHTML = data;
 
         let headerContent = tempDiv.querySelector("header");
-        console.log(headerContent)
         if (headerContent) {
             document.getElementById("headerCesta").innerHTML = headerContent.innerHTML;
     }
 });
+
+let contenedorCompra = document.querySelector(".contenedorCompra");
+let contenedorCursos = document.querySelector(".contenedorCursos");
+let contenedorDatos = document.querySelector('.contenedorDatos');
+let contenedorPagar = document.querySelector('.contenedorPagar');
+let carrito_compra = JSON.parse(localStorage.getItem("carrito")) || [];
+let total;
+document.addEventListener('DOMContentLoaded', verCesta);
+
+function verCesta(){
+    carrito_compra.forEach(curso => {
+        const {imagen,titulo,precio,autor} = curso;
+        total += precio;
+        let divCursoImg = document.createElement('DIV');
+        divCursoImg.classList.add('detallesCurso');
+        divCursoImg.innerHTML =`
+        <img src="${imagen}" id="imgCesta">
+        <div class="informacion">
+            <p class="titulo">${titulo}</p>
+            <p>${autor}</p>
+        </div>
+        <div class='opciones'>
+            <p>Eliminar</p>
+            <p>Guardar para despues</p>
+            <p>Mover a la lista de deseos</p>
+        </div>
+        <p class="precioCesta">${precio}</p>
+        `
+        contenedorCursos.appendChild(divCursoImg);
+    });
+    
+}
